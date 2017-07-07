e1931e31e6a43d380ab938c42386b2e299611e69
Madriax <madriax.gamer@gmail.com>
2017-07-07 20:17:47 +0200

    Deposit and withdraw clean and dirty money

--- apart_server.lua
+++ apart_server.lua
@@ -5,7 +5,7 @@
 --MySQL   -----------------
 --Couchdb ----------------- (soon)
 ---------------------------
-local mode = "MySQL"
+local mode = "Async"
 
 if (mode == "Async") then
   require "resources/mysql-async/lib/MySQL"
@@ -14,7 +14,7 @@ elseif mode == "MySQL" then
   require "resources/essentialmode/lib/MySQL"
 
   --DO NOT FORGET TO CHANGE THE LINE BELLOW !!!
-  MySQL:open("localhost", "DATABASE (default: gta5_gamemode_essential)", "USERNAME", "PASSWORD")
+  MySQL:open("localhost", "gta5_gamemode_essential", "root", "Police911")
 end
 
 
@@ -35,6 +35,8 @@ local txt = {
 
 
 local isBuy = 0
+local money = 0
+local dirtymoney = 0
 
 RegisterServerEvent("apart:getAppart")
 AddEventHandler('apart:getAppart', function(name)
@@ -81,6 +83,181 @@ AddEventHandler('apart:getAppart', function(name)
   end)
 end)
 
+RegisterServerEvent("apart:getCash")
+AddEventHandler('apart:getCash', function(name)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local name = name
+    if (mode == "Async") then
+      MySQL.Async.fetchAll("SELECT * FROM user_appartement WHERE name = @nom", {['@nom'] = tostring(name)}, function (result)
+        if (result) then
+          money = result[1].money
+          dirtymoney = result[1].dirtymoney
+          TriggerClientEvent('apart:getCash', source, money, dirtymoney)
+        end
+      end)
+    elseif mode == "MySQL" then
+      local executed_query = MySQL:executeQuery("SELECT * FROM user_appartement WHERE name = @nom", {['@nom'] = tostring(name)})
+      local result = MySQL:getResults(executed_query, {'identifier'})
+      if (result) then
+        money = result[1].money
+        dirtymoney = result[1].dirtymoney
+        TriggerClientEvent('apart:getCash', source, money, dirtymoney)
+      end
+    end
+  end)
+end)
+
+RegisterServerEvent("apart:depositcash")
+AddEventHandler('apart:depositcash', function(cash, apart)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local money = 0
+    if (tonumber(user.money) >= tonumber(cash) and tonumber(cash) > 0) then
+      if mode == "Async" then
+        MySQL.Async.fetchAll("SELECT money FROM user_appartement WHERE name = @nom", {['@nom'] = apart}, function (result)
+            if (result) then
+              money = result[1].money
+              user:removeMoney((cash))
+              local newmoney = money + cash
+              MySQL.Async.execute("UPDATE user_appartement SET `money`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart}, function(data)
+              end)
+            end
+        end)
+      elseif mode == "MySQL" then
+        local executed_query = MySQL:executeQuery("SELECT money FROM user_appartement WHERE name = @nom", {['@nom'] = apart})
+        local result = MySQL:getResults(executed_query, {'money'})
+        if (result) then
+          money = result[1].money
+          user:removeMoney((cash))
+          local newmoney = money + cash
+          MySQL:executeQuery("UPDATE user_appartement SET `money`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart})
+        end
+      end
+
+      TriggerClientEvent('apart:getCash', source, money, dirtymoney)
+    else
+      -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+      TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
+    end
+  end)
+end)
+
+RegisterServerEvent("apart:depositdirtycash")
+AddEventHandler('apart:depositdirtycash', function(cash, apart)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local money = 0
+    if (tonumber(user:getDMoney()) >= tonumber(cash) and tonumber(cash) > 0) then
+      if mode == "Async" then
+        MySQL.Async.fetchAll("SELECT dirtymoney FROM user_appartement WHERE name = @nom", {['@nom'] = apart}, function (result)
+            if (result) then
+              money = result[1].dirtymoney
+              user:removeDMoney((cash))
+              local newmoney = money + cash
+              MySQL.Async.execute("UPDATE user_appartement SET `dirtymoney`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart}, function(data)
+              end)
+            end
+        end)
+      elseif mode == "MySQL" then
+        local executed_query = MySQL:executeQuery("SELECT dirtymoney FROM user_appartement WHERE name = @nom", {['@nom'] = apart})
+        local result = MySQL:getResults(executed_query, {'dirtymoney'})
+        if (result) then
+          money = result[1].dirtymoney
+          user:removeDMoney((cash))
+          local newmoney = money + cash
+          MySQL:executeQuery("UPDATE user_appartement SET `dirtymoney`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart})
+        end
+      end
+
+      TriggerClientEvent('apart:getCash', source, money, dirtymoney)
+    else
+      -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+      TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
+    end
+  end)
+end)
+
+RegisterServerEvent("apart:takecash")
+AddEventHandler('apart:takecash', function(cash, apart)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local money = 0
+    if mode == "Async" then
+      MySQL.Async.fetchAll("SELECT money FROM user_appartement WHERE name = @nom", {['@nom'] = apart}, function (result)
+          if (result) then
+            money = result[1].money
+            if (tonumber(cash) <= tonumber(money) and tonumber(cash) > 0) then
+              user:addMoney((cash))
+              local newmoney = money - cash
+              MySQL.Async.execute("UPDATE user_appartement SET `money`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart}, function(data)
+              end)
+            else
+              -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+              TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
+            end
+          end
+      end)
+    elseif mode == "MySQL" then
+      local executed_query = MySQL:executeQuery("SELECT money FROM user_appartement WHERE name = @nom", {['@nom'] = apart})
+      local result = MySQL:getResults(executed_query, {'money'})
+      if (result) then
+        money = result[1].money
+        if (tonumber(cash) <= tonumber(money) and tonumber(cash) > 0) then
+          user:addMoney((cash))
+          local newmoney = money - cash
+          MySQL:executeQuery("UPDATE user_appartement SET `money`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart})
+        else
+          -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+          TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
+        end
+      end
+    end
+
+    TriggerClientEvent('apart:getCash', source, money, dirtymoney)
+  end)
+end)
+
+RegisterServerEvent("apart:takedirtycash")
+AddEventHandler('apart:takedirtycash', function(cash, apart)
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local money = 0
+    if mode == "Async" then
+      MySQL.Async.fetchAll("SELECT dirtymoney FROM user_appartement WHERE name = @nom", {['@nom'] = apart}, function (result)
+          if (result) then
+            money = result[1].money
+            if (tonumber(cash) <= tonumber(money) and tonumber(cash) > 0) then
+              user:addDMoney((cash))
+              local newmoney = money - cash
+              MySQL.Async.execute("UPDATE user_appartement SET `dirtymoney`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart}, function(data)
+              end)
+            else
+              -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+              TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
+            end
+          end
+      end)
+    elseif mode == "MySQL" then
+      local executed_query = MySQL:executeQuery("SELECT dirtymoney FROM user_appartement WHERE name = @nom", {['@nom'] = apart})
+      local result = MySQL:getResults(executed_query, {'dirtymoney'})
+      if (result) then
+        money = result[1].money
+        if (tonumber(cash) <= tonumber(money) and tonumber(cash) > 0) then
+          user:addDMoney((cash))
+          local newmoney = money - cash
+          MySQL:executeQuery("UPDATE user_appartement SET `dirtymoney`=@cash WHERE name = @nom",{['@cash'] = newmoney, ['@nom'] = apart})
+        else
+          -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+          TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
+        end
+      end
+    end
+
+    TriggerClientEvent('apart:getCash', source, money, dirtymoney)
+  end)
+end)
+
 RegisterServerEvent("apart:buyAppart")
 AddEventHandler('apart:buyAppart', function(name, price)
   TriggerEvent('es:getPlayerFromId', source, function(user)
@@ -94,10 +271,12 @@ AddEventHandler('apart:buyAppart', function(name, price)
       elseif mode == "MySQL" then
         local executed_query2 = MySQL:executeQuery("INSERT INTO user_appartement (`identifier`, `name`, `price`) VALUES (@username, @name, @price)", {['@username'] = player, ['@name'] = name, ['@price'] = price})
       end
-    	TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['welcome'])
+      -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['welcome']) --(FOR FREEROAM)
+    	TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['welcome']) --WITH LALIFE SCRIPTS
     	TriggerClientEvent('apart:isMine', source)
     else
-    	TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash'])
+    	-- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --(FOR FREEROAM)
+    	TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['nocash']) --WITH LALIFE SCRIPTS
     end
   end)
 end)
@@ -116,7 +295,8 @@ AddEventHandler('apart:sellAppart', function(name, price)
         local executed_query3 = MySQL:executeQuery("DELETE from user_appartement WHERE identifier = @username AND name = @name",
         {['@username'] = player, ['@name'] = name})
       end
-      TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['estVendu'])
+      -- TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['estVendu']) --(FOR FREEROAM)
+      TriggerClientEvent("citizenv:notify", source, "CHAR_SIMEON", 1, "Stephane", false, txt[lang]['estVendu']) --WITH LALIFE SCRIPTS
       TriggerClientEvent('apart:isNotBuy', source)
   end)
 end)
